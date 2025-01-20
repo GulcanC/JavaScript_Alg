8490fa7a214abf0ed125376c76210481b5fb491c
Gulcan COSKUN <gulcancoskun.a@gmail.com>
2025-01-20 11:04:58 +0100

    add golf code

---
+++ alg_1.js
@@ -0,0 +1,30 @@
+
+// 8 different data types
+
+// undefined, null, boolean, string, symbol, bigint, number, object
+
+// operators += and -=
+
+let sum = 1;
+sum = sum + 5;
+console.log(sum); // 6
+
+
+let tot =2;
+tot += 9;
+console.log(tot); // 11
+
+///////////////////////////////////////////////////////////////////
+
+let sub = 5;
+sub = sub - 5;
+console.log(sub); // 0
+
+let x = 2;
+x -= 10;
+console.log(x); // -8
+
+const myStr = "FirstLine\n\t\SecondLine\nThirdLine";
+console.log(myStr)
+
+// GOLF CODE // 
